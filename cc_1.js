81a8286874b6c5a4839ddb736d540f6336178d07
Lukeguy334 <lhs57@usf.edu>
2025-01-24 21:18:05 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employee.name = "John Doe";
+const employeeID = 12345
+var isActive = true;
+
+console.log("John Doe", employee.name, "| Type;", typeof employee.name);
+console.log("12345", employeeID, "| Type;", typeof employeeID);
+console.log("true", isActive, "| Type;", typeof isActive);
