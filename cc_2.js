d4bc4a338ca1bbf04c90b289d79ac4d313001a84
Lukeguy334 <lhs57@usf.edu>
2025-01-26 18:36:27 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,12 @@ products.push("Smart TV");
 products.pop();
 console.log("Updated Product List", products);
 
+let scores = [85, 90, 78, 92, 88];
+scores[1] = 95;
+let total = 0;
+for (let i = 0; i < scores.length; i++) {
+    total += scores[i];
+}
+let average = total / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Score:", average.toFixed(2));
