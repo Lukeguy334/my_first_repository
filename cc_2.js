5a1558a878c3888d4631234894f0155e7224d516
Lukeguy334 <lhs57@usf.edu>
2025-01-26 18:33:06 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+let products = ["Wireless Headphones", "Smartphone", "Laptop", "Tablet", "Smartwatch"];
+products.push("Smart TV");
+products.pop();
+console.log("Updated Product List", products);
+
