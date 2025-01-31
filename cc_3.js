d066f1f74121a161b3626ab7c0774e03cd670a8e
Lukeguy334 <lhs57@usf.edu>
2025-01-31 00:17:10 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,13 @@
 // setting prices
-let Prices = [25, 30, 35, 45, 60]
+let Prices = [25, 30, 35, 45, 60];
 Prices.push(70); // add new price to array
-Prices.slice(1); // remove price from array
+Prices.shift(); // remove price from array
 console.log("Updated Prices", Prices);
+
+// setting & altering stock
+let Instock = [10, 15, 20, 30, 40];
+Instock [3] +=5; // add 5 to stock
+let totalStock = Instock.reduce((sum, num) => sum + num, 0);
+console.log("Updated Stock:", Instock);
+console.log("Total Stock:", totalStock); // total stock 120
+
