5835909fa46e46c504b3ad44ca00f1b98952aae9
Lukeguy334 <lhs57@usf.edu>
2025-01-31 19:55:38 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,12 @@ if (purchaseAmount > 100) {
 }
 console.log("Final Purchase Amount:", purchaseAmount);
 
+
+// calculate sales
+let sales = [50, 75, 100, 125, 150];
+let totalSales = 0
+for (let i = 0; i < sales.length; i++) { // calculating total sales
+    totalSales += sales[i];
+}
+console.log("Updated Sales:", sales);
+console.log("Total Sales:", totalSales); // 500
